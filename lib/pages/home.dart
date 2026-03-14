f064f95d7490e81224bbba3223ab975a24982782
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-03-14 21:58:32 +0500

    "App Bar finished"

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,27 +7,60 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "BreakFast",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "Fuzail Project",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
           margin: EdgeInsets.all(10),
-          child: SvgPicture.asset(),
+          alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: Colors.black,
+            color: Color(0xffF7F8F8),
             borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
           ),
         ),
       ),
+
+      actions: [
+        GestureDetector(
+          onTap: () {},
+
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
